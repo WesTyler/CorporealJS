17d7580bfd892c32e2fd879d51db97afa95e1448
WesTyler <westyler5@gmail.com>
2015-07-13 18:57:50 -0500

    Include config.js for Heroku

---
+++ server/config.js
@@ -0,0 +1,6 @@
+module.exports={}
+
+module.exports.pass = '9f891e2e';
+module.exports.user = 'bac1cb3a2b8c2a';
+module.exports.host = 'us-cdbr-iron-east-02.cleardb.net';
+module.exports.database = 'heroku_d758e10738131e0';
