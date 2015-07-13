2dbb974fb5bfcee03da4564ccdd94e53147770ab
WesTyler <westyler5@gmail.com>
2015-07-13 15:28:50 -0500

    Post detail views allow navigation to previous/next posts if there is one. Initial page load GETs posts and shows summaries.

--- client/posts.js
+++ client/posts.js
@@ -11,8 +11,8 @@ Posts.model = function () {
 Posts.controller = function () {
   var ctrl = this;
   ctrl.posts = m.prop([]); //initialize as empty array of posts
-  ctrl.viewMode = m.prop('');
-  ctrl.selectedPost = m.prop(0);
+  ctrl.viewMode = m.prop(''); //determine which page to view
+  ctrl.selectedPost = m.prop(0); //determine which post to view in detail
 
   ctrl.add = function(postObj) {
     //check ctrl.posts to see if the post is already in posts
@@ -33,37 +33,44 @@ Posts.controller = function () {
     };
   };
 
-  ctrl.remove = function(idx) {
-    ctrl.posts().splice(idx,1);
-  };
-
-  ctrl.get = function(){
+  ctrl.get = function(){ //Make GET request to server to retrieve posts
     m.request({method: 'GET', url: '/posts'})
     .then(ctrl.postBuilder);
   };
 
-  ctrl.postBuilder = function(posts){
+  ctrl.postBuilder = function(posts){ //Iterate over server response to add new posts to the page
     for (var i=0; i<posts.results.length; i++) {
       ctrl.viewMode('summary');
       ctrl.add(posts.results[i]);
     }
   };
 
-  ctrl.aboutMode = function() {
+  ctrl.aboutMode = function() { //View the 'About' page
     ctrl.viewMode('about');
   };
 
-  ctrl.detailMode = function() {
+  ctrl.detailMode = function() { //View the selected post's detail page
     ctrl.selectedPost(this.id);
     ctrl.viewMode('detail');
-  }
+  };
+
+  ctrl.loadPrev = function() { //View previous post
+    var temp = ctrl.selectedPost();
+    temp--;
+    ctrl.selectedPost(temp);
+  };
+
+  ctrl.loadNext = function() { //View next post
+    var temp = ctrl.selectedPost();
+    temp++;
+    ctrl.selectedPost(temp);
+  };
 }
 
 Posts.view = function (ctrl) {
-  console.log('rendering view')
   return m('.app', [
     
-    m('nav.clearfix', [
+    m('nav.clearfix', [ //Nav bar and my social media links
       m('.social', [
         m('a.follow', {id: 'twitter', target:'_blank', href: 'https://twitter.com/westyler1'}, 'twitter'),
         m('a.follow', {id: 'github', target:'_blank', href: 'https://github.com/WesTyler'}, 'github'),
@@ -75,7 +82,7 @@ Posts.view = function (ctrl) {
         ]),
     ]),
     
-    checkMode(ctrl)
+    checkMode(ctrl) //Check which body content type to display
   ])
 }
 
@@ -91,7 +98,15 @@ function checkMode(ctrl) {
     return m('div', {class: 'detailView clearfix'}, [
       m('h3.detail', ctrl.posts()[ctrl.selectedPost()-1].title()),
       m('h5.author', ctrl.posts()[ctrl.selectedPost()-1].author()),
-      m('p.text', ctrl.posts()[ctrl.selectedPost()-1].content())
+      m('p.text', ctrl.posts()[ctrl.selectedPost()-1].content()),
+      function() {
+        if (ctrl.posts()[ctrl.selectedPost()-2]) {
+          return m('a.prev', {href: '#', onclick: ctrl.loadPrev}, 'Previous Post')
+        };
+        if (ctrl.posts()[ctrl.selectedPost()]) {
+          return m('a.next', {href: '#', onclick: ctrl.loadNext}, 'Next Post')
+        };        
+      }()
     ]);
   } else if (ctrl.viewMode()==='about') {
     return m('div', {class: 'aboutView clearfix'}, [
@@ -102,5 +117,8 @@ function checkMode(ctrl) {
         m('p.about', "This blog was built using Mithril, Node/Express, and MySQL.")
       ]) 
     ])
-  };
+  } else {
+    ctrl.viewMode('summary');
+    ctrl.get();
+  }
 }
